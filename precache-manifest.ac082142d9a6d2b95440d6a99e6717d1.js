60261744d213d8c3373b7c6e93638116fb9d1910
jay <dyjay15@gmail.com>
2020-08-27 11:15:52 +0800

    Updates

---
+++ precache-manifest.ac082142d9a6d2b95440d6a99e6717d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877e9ba36d7c2023caf1b3ed229b0947",
+    "url": "/wanderlustbakeshop/index.html"
+  },
+  {
+    "revision": "8c9461f4de94540659fc",
+    "url": "/wanderlustbakeshop/static/css/main.f35ea0bf.chunk.css"
+  },
+  {
+    "revision": "79d435f6d779ccb06e2d",
+    "url": "/wanderlustbakeshop/static/js/2.a28afb6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wanderlustbakeshop/static/js/2.a28afb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9461f4de94540659fc",
+    "url": "/wanderlustbakeshop/static/js/main.0238b443.chunk.js"
+  },
+  {
+    "revision": "c61791c3e66db15d888e",
+    "url": "/wanderlustbakeshop/static/js/runtime-main.3a7d906c.js"
+  },
+  {
+    "revision": "4d919399e5f9c51d3e32f1dd8ed02d02",
+    "url": "/wanderlustbakeshop/static/media/6.4d919399.jpg"
+  },
+  {
+    "revision": "950dea7fde93cbbf1fbc6cb368cec0ea",
+    "url": "/wanderlustbakeshop/static/media/COOKIES.950dea7f.jpg"
+  },
+  {
+    "revision": "1edbea164b5dbeb004ef94e8b726d7f3",
+    "url": "/wanderlustbakeshop/static/media/Puff Menu.1edbea16.jpg"
+  },
+  {
+    "revision": "3a66c402d0abf73567114470c4ce60d6",
+    "url": "/wanderlustbakeshop/static/media/Tagline.3a66c402.jpg"
+  },
+  {
+    "revision": "beb355f754437a8636c47c40e1f9cd45",
+    "url": "/wanderlustbakeshop/static/media/image00022.beb355f7.jpeg"
+  },
+  {
+    "revision": "860b9123ff69e357f6e28a61ea7f38cd",
+    "url": "/wanderlustbakeshop/static/media/puff nation.860b9123.png"
+  }
+]);
